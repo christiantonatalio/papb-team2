3382880eec0fd365001ba65aa92039a0e68d023c
natal <christianto.natalio.saleky@mail.ugm.ac.id>
2021-10-12 19:46:40 +0700

    Beverage Adapter fixed

--- app/src/main/java/com/example/foodapp/ui/beverage/BeverageAdapter.kt
+++ app/src/main/java/com/example/foodapp/ui/beverage/BeverageAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.foodapp.ui.beverage
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.foodapp.R
 import com.example.foodapp.databinding.ItemBeverageBinding
 import com.example.foodapp.model.Beverage
@@ -25,10 +28,16 @@ class BeverageAdapter (private val listBeverage: ArrayList<Beverage>) : Recycler
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val beverage = listBeverage[position]
 
+        Glide.with(holder.itemView.context)
+            .load(beverage.image)
+            .apply(RequestOptions().override(100, 100))
+            .into(holder.binding.imgGambar)
 
+        holder.binding.txtName.text = beverage.name
+        holder.binding.txtDescrption.text = beverage.description
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listBeverage.size
     }
 }
