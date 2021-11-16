f9ceb0098a9e09d929b65442abc2e32a9439ceea
AttarHusnaFathiya <attarhusnafathiya@gmail.com>
2021-11-16 21:04:33 +0700

    add food annotation

--- app/src/main/java/com/example/foodapp/model/Food.kt
+++ app/src/main/java/com/example/foodapp/model/Food.kt
@@ -1,7 +1,20 @@
 package com.example.foodapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "food_table")
 data class Food (
+    @PrimaryKey(autoGenerate = true)
+    var FoodId: Int = 0,
+
+    @ColumnInfo(name = "food_column_name")
     var name: String? = "",
+
+    @ColumnInfo(name = "food_column_description")
     var description: String? = "",
+
+    @ColumnInfo(name = "food_column_image")
     var image: Int? = 0
 )
